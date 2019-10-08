4b7b918ef8f49efe6fd9e8eaf9feabbd704a95cd
Drupal Infrastructure <engineering@association.drupal.org>
2019-10-08 22:24:27 +0000

    Update composer.json ()
    Reference: https://git.drupalcode.org/project/drupal/commit/9b7ead2e45935bc1dadfe74490b8cbefa54f433a

--- assets/example.settings.local.php
+++ assets/example.settings.local.php
@@ -5,84 +5,20 @@
  * Local development override configuration feature.
  *
  * To activate this feature, copy and rename it such that its path plus
- * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
- * 'sites/default/settings.php' and uncomment the commented lines that mention
- * 'settings.local.php'.
- *
- * If you are using a site name in the path, such as 'sites/example.com', copy
- * this file to 'sites/example.com/settings.local.php', and uncomment the lines
- * at the bottom of 'sites/example.com/settings.php'.
+ * filename is 'sites/yoursite.com/settings.local.php'. Then, go to the bottom
+ * of 'sites/yoursite.com/settings.php' and uncomment the commented lines that
+ * mention 'settings.local.php'.
  */
 
-/**
- * Assertions.
- *
- * The Drupal project primarily uses runtime assertions to enforce the
- * expectations of the API by failing when incorrect calls are made by code
- * under development.
- *
- * @see http://php.net/assert
- * @see https://www.drupal.org/node/2492225
- *
- * If you are using PHP 7.0 it is strongly recommended that you set
- * zend.assertions=1 in the PHP.ini file (It cannot be changed from .htaccess
- * or runtime) on development machines and to 0 in production.
- *
- * @see https://wiki.php.net/rfc/expectations
- */
-assert_options(ASSERT_ACTIVE, TRUE);
-\Drupal\Component\Assertion\Handle::register();
-
-/**
- * Enable local development services.
- */
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-
-/**
- * Show all error messages, with backtrace information.
- *
- * In case the error level could not be fetched from the database, as for
- * example the database connection failed, we rely only on this value.
- */
+// Show all error messages, with backtrace information.
 $config['system.logging']['error_level'] = 'verbose';
 
-/**
- * Disable CSS and JS aggregation.
- */
+// Disable CSS and JS aggregation.
 $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
-/**
- * Disable the render cache (this includes the page cache).
- *
- * Note: you should test with the render cache enabled, to ensure the correct
- * cacheability metadata is present. However, in the early stages of
- * development, you may want to disable it.
- *
- * This setting disables the render cache by using the Null cache back-end
- * defined by the development.services.yml file above.
- *
- * Do not use this setting until after the site is installed.
- */
-# $settings['cache']['bins']['render'] = 'cache.backend.null';
-
-/**
- * Disable Dynamic Page Cache.
- *
- * Note: you should test with Dynamic Page Cache enabled, to ensure the correct
- * cacheability metadata is present (and hence the expected behavior). However,
- * in the early stages of development, you may want to disable it.
- */
-# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-
-/**
- * Allow test modules and themes to be installed.
- *
- * Drupal ignores test modules and themes by default for performance reasons.
- * During development it can be useful to install test extensions for debugging
- * purposes.
- */
-$settings['extension_discovery_scan_tests'] = TRUE;
+// Disable the render cache, by using the Null cache back-end.
+$settings['cache']['bins']['render'] = 'cache.backend.null';
 
 /**
  * Enable access to rebuild.php.
@@ -93,3 +29,56 @@ $settings['extension_discovery_scan_tests'] = TRUE;
  * using these parameters in a request to rebuild.php.
  */
 $settings['rebuild_access'] = TRUE;
+
+/**
+ * Twig debugging:
+ *
+ * When debugging is enabled:
+ * - The markup of each Twig template is surrounded by HTML comments that
+ *   contain theming information, such as template file name suggestions.
+ * - Note that this debugging markup will cause automated tests that directly
+ *   check rendered HTML to fail. When running automated tests, 'twig_debug'
+ *   should be set to FALSE.
+ * - The dump() function can be used in Twig templates to output information
+ *   about template variables.
+ * - Twig templates are automatically recompiled whenever the source code
+ *   changes (see twig_auto_reload below).
+ *
+ * Note: changes to this setting will only take effect once the cache is
+ * cleared.
+ *
+ * For more information about debugging Twig templates, see
+ * http://drupal.org/node/1906392.
+ *
+ * Not recommended in production environments (Default: FALSE).
+ */
+# $settings['twig_debug'] = TRUE;
+
+/**
+ * Twig auto-reload:
+ *
+ * Automatically recompile Twig templates whenever the source code changes. If
+ * you don't provide a value for twig_auto_reload, it will be determined based
+ * on the value of twig_debug.
+ *
+ * Note: changes to this setting will only take effect once the cache is
+ * cleared.
+ *
+ * Not recommended in production environments (Default: NULL).
+ */
+# $settings['twig_auto_reload'] = TRUE;
+
+/**
+ * Twig cache:
+ *
+ * By default, Twig templates will be compiled and stored in the filesystem to
+ * increase performance. Disabling the Twig cache will recompile the templates
+ * from source each time they are used. In most cases the twig_auto_reload
+ * setting above should be enabled rather than disabling the Twig cache.
+ *
+ * Note: changes to this setting will only take effect once the cache is
+ * cleared.
+ *
+ * Not recommended in production environments (Default: TRUE).
+ */
+# $settings['twig_cache'] = FALSE;
--- assets/index.php
+++ assets/index.php
@@ -9,14 +9,32 @@
  */
 
 use Drupal\Core\DrupalKernel;
+use Drupal\Core\Site\Settings;
 use Symfony\Component\HttpFoundation\Request;
 
-$autoloader = require_once 'autoload.php';
+$autoloader = require_once __DIR__ . '/core/vendor/autoload.php';
 
-$kernel = new DrupalKernel('prod', $autoloader);
+try {
 
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
+  $request = Request::createFromGlobals();
+  $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
+  $response = $kernel
+    ->handlePageCache($request)
+    ->handle($request)
+      // Handle the response object.
+      ->prepare($request)->send();
+  $kernel->terminate($request, $response);
+}
+catch (Exception $e) {
+  $message = 'If you have just changed code (for example deployed a new module or moved an existing one) read <a href="http://drupal.org/documentation/rebuild">http://drupal.org/documentation/rebuild</a>';
+  if (Settings::get('rebuild_access', FALSE)) {
+    $rebuild_path = $GLOBALS['base_url'] . '/rebuild.php';
+    $message .= " or run the <a href=\"$rebuild_path\">rebuild script</a>";
+  }
 
-$kernel->terminate($request, $response);
+  // Set the response code manually. Otherwise, this response will default to a
+  // 200.
+  http_response_code(500);
+  print $message;
+  throw $e;
+}
